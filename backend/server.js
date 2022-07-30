c4a2e92c394d23ee84c32c20f64e876caa7bd74c
Ben Stones <benstones99@gmail.com>
2022-07-30 20:41:35 +0100

    Added connection to mongodb server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 require('dotenv').config();
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true })
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Connection to MongoDB established successfully!')
+})
 
 app.use(cors());
 app.use(express.json());
