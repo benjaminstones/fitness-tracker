0ea2c2cd95a52f0059db362cc1fb4af30a62014c
Ben Stones <benstones99@gmail.com>
2022-08-01 21:01:15 +0100

    Added create use rbasic

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "react-datepicker/dist/react-datepicker.css";
 
 const CreateUser = () => {
-    return (
-        <p>create user</p>
-    )
+const [username, setUsername] = useState('');
+
+const updateUsername = e => setUsername(e.target.value);
+
+const onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      username: username
+    }
+
+    console.log(user);
+
+  }
+
+return (
+    <div>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group"> 
+          <label>Username: </label>
+          <input  type="text"
+              required
+              className="form-control"
+              value={username}
+              onChange={updateUsername}
+              />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Create User" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default CreateUser;
