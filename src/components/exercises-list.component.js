3a9e809e07e96c8e8b6313e0ca32c085a149e6a3
Ben Stones <benstones99@gmail.com>
2022-08-05 15:11:31 +0100

    Tidy up

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -8,45 +8,45 @@ const ExercisesList = () => {
 
     useEffect(() => {
         axios.get('http://localhost:5001/exercises/')
-        .then(res => {
-            setExercises( res.data )
-            console.log(exercises);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .then(res => {
+                setExercises(res.data)
+                console.log(exercises);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }, [])
 
     const deleteExercise = (id) => {
-        axios.delete('http://localhost:5001/exercises/'+id)
-          .then(response => { console.log(response.data)});
+        axios.delete('http://localhost:5001/exercises/' + id)
+            .then(response => { console.log(response.data) });
         setExercises(exercises.filter(el => el._id !== id))
     }
-    
+
     return (
         <>
-        <h3>Logged Exercises</h3>
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Username</th>
-              <th>Description</th>
-              <th>Duration</th>
-              <th>Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            { exercises.map(exercise => (
-                    <tr key={exercise._id} >
-                        <td>{exercise.username}</td>
-                        <td>{exercise.description}</td>
-                        <td>{exercise.duration}</td>
-                        <td>{exercise.date.substring(0,10)}</td>
+            <h3>Logged Exercises</h3>
+            <table className="table">
+                <thead className="thead-light">
+                    <tr>
+                        <th>Username</th>
+                        <th>Description</th>
+                        <th>Duration</th>
+                        <th>Date</th>
+                        <th>Actions</th>
                     </tr>
-            )) }
-          </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {exercises.map(exercise => (
+                        <tr key={exercise._id} >
+                            <td>{exercise.username}</td>
+                            <td>{exercise.description}</td>
+                            <td>{exercise.duration}</td>
+                            <td>{exercise.date.substring(0, 10)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
