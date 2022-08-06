d9f69d6fe7e44d920427abf52e943586a5460389
Ben Stones <benstones99@gmail.com>
2022-08-06 23:59:44 +0100

    more clean up

--- src/components/createExercise.js
+++ src/components/createExercise.js
@@ -4,22 +4,22 @@ import axios from 'axios';
 import { useState, useEffect } from "react";
 
 const CreateExercise = () => {
-    const [username, setUsername] = useState('');
-    const [description, setDescription] = useState('');
-    const [duration, setDuration] = useState(0);
-    const [date, setDate] = useState(new Date());
-    const [users, setUsers] = useState([])
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([])
 
-    useEffect(() => {
-      axios.get('http://localhost:5001/users').then(res => {
-        if (res.data.length > 0) {
-          setUsers(res.data.map(user => user.username))
-          setUsername(res.data[0].username)
-        }
-      }).catch((error) => {
-        console.log(error);
-      })
-    },[]);
+  useEffect(() => {
+    axios.get('http://localhost:5001/users').then(res => {
+      if (res.data.length > 0) {
+        setUsers(res.data.map(user => user.username))
+        setUsername(res.data[0].username)
+      }
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, []);
 
 
   const updateUsername = e => setUsername(e.target.value);
@@ -35,59 +35,59 @@ const CreateExercise = () => {
     }
     axios.post('http://localhost:5001/exercises/add', exercise).then(res => console.log(res.data))
   }
-    return (
-        <div>
-          <h3>Create New Exercise Log</h3>
-          <form onSubmit={onSubmit}>
-            <div className="form-group"> 
-              <label>Username: </label>
-              <select type="text" 
-                  required
-                  className="form-control"
-                  onChange={updateUsername}
-                >
-                  {
-                    users.map((user) => {
-                      return <option 
-                        key={user}
-                        value={user}>{user}
-                        </option>;
-                    })
-                  }
-              </select>
-            </div>
-            <div className="form-group"> 
-              <label>Description: </label>
-              <input  type="text"
-                  required
-                  className="form-control"
-                   onChange={updateDescription}
-                  />
-            </div>
-            <div className="form-group">
-              <label>Duration (in minutes): </label>
-              <input 
-                  type="text" 
-                  className="form-control"
-                  onChange={updateDuration}
-                  />
-            </div>
-            <div className="form-group">
-              <label>Date: </label>
-              <div>
-                <DatePicker
-                  selected={date}
-                  onChange={(date) => setDate(date)}
-                />
-              </div>
-            </div>
-    
-            <div className="form-group">
-              <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
-            </div>
-          </form>
+  return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select type="text"
+            required
+            className="form-control"
+            onChange={updateUsername}
+          >
+            {
+              users.map((user) => {
+                return <option
+                  key={user}
+                  value={user}>{user}
+                </option>;
+              })
+            }
+          </select>
         </div>
-        )
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text"
+            required
+            className="form-control"
+            onChange={updateDescription}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={updateDuration}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={date}
+              onChange={(date) => setDate(date)}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default CreateExercise;
