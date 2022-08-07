f11710c34c975eb249225949c0b4e138b42c6307
Ben Stones <benstones99@gmail.com>
2022-08-07 17:28:09 +0100

    Added exercise schema

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  notes: { type: String, required: false },
+  musclegroup: { type: String, required: false}
+}, {
+  timestamps: true,
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
