46727cb5ffb6e1259c0ba5212bad731c1e26338b
Ben Stones <benstones99@gmail.com>
2022-08-07 19:07:58 +0100

    Added exercises to workout request

--- backend/models/workout.model.js
+++ backend/models/workout.model.js
@@ -6,6 +6,7 @@ const workoutsSchema = new Schema({
   username: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
+  exercises: { type: Array, required: true },
   date: { type: Date, required: true },
 }, {
   timestamps: true,
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -11,12 +11,14 @@ router.route('/add').post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
+  const exercises = (req.body.exercises)
   const date = Date.parse(req.body.date);
 
   const newWorkout = new Workout({
     username,
     description,
     duration,
+    exercises,
     date,
   });
 
