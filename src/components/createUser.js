02e1596a3460b7ba4539c4e95d61fb7b95279024
Ben Stones <benstones99@gmail.com>
2022-08-08 22:08:57 +0100

    antd form to create user

--- src/components/createUser.js
+++ src/components/createUser.js
@@ -1,43 +1,59 @@
 import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
 import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 
 const CreateUser = () => {
-const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
 
-const updateUsername = e => setUsername(e.target.value);
+    const updateUsername = e => setUsername(e.target.value);
 
-const onSubmit = e => {
-    e.preventDefault();
-    const user = {
-      username: username
+    const onSubmit = e => {
+        // e.preventDefault();
+        const user = {
+            username: username
+        }
+
+        axios.post('http://localhost:5001/users/add', user).then(res => console.log(res.data))
+
+        console.log(user);
+        return false;
     }
 
-    axios.post('http://localhost:5001/users/add', user).then(res => console.log(res.data))
-
-    console.log(user);
-
-  }
-
-return (
-    <div>
-      <h3>Create New User</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-group"> 
-          <label>Username: </label>
-          <input  type="text"
-              required
-              className="form-control"
-              value={username}
-              onChange={updateUsername}
-              />
-        </div>
-        <div className="form-group">
-          <input type="submit" value="Create User" className="btn btn-primary" />
-        </div>
-      </form>
-    </div>
-  )
+    return (
+        <>
+            <h3>Create New User</h3>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                onFinish={onSubmit}
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input
+                        value={username}
+                        onChange={updateUsername}
+                    />
+                </Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
 }
 
 export default CreateUser;
