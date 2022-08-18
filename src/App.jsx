e6b9be6025444ada6f422cce53475654239f4b8c
Ben Stones <benstones99@gmail.com>
2022-08-18 23:12:49 +0100

    Fixed app routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import WorkoutsList from "./components/workoutsList";
 import EditWorkout from "./components/editWorkout";
 import CreateWorkout from "./components/createWorkout";
 import CreateUser from "./components/createUser";
+import CreateExercise from './components/createExercise';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/edit/:id" element={<EditWorkout />} />
         <Route path="/create" element={<CreateWorkout />} />
         <Route path="/user" element={<CreateUser />} />
+        <Route path="/exercise" element={<CreateExercise />} />
       </Routes>
     </div>
   );
