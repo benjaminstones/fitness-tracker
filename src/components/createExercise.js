f9cf52caad8cb7a91c81c1637321175380853ccf
Ben Stones <benstones99@gmail.com>
2022-08-18 23:11:55 +0100

    Added create exercise page

---
+++ src/components/createExercise.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
+import axios from 'axios';
+
+const CreateExercise = () => {
+    const [name, setName] = useState('');
+    const [musclegroup, setMusclegroup] = useState('');
+    const [notes, setNotes] = useState('');
+
+    const updateName = e => setName(e.target.value);
+    const updateMusclegroup = e => setMusclegroup(e.target.value);
+    const updateNotes = e => setNotes(e.target.value);
+
+
+    const onSubmit = e => {
+        const exercise = {
+            name: name,
+            musclegroup: musclegroup,
+            notes: notes
+        }
+        axios.post('http://localhost:5001/exercises/add', exercise).then(res => console.log(res.data))
+
+        console.log(exercise);
+        return false;
+    }
+
+    return (
+        <>
+            <h3>Create New Exercise</h3>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                onFinish={onSubmit}
+            >
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input an exercise name!',
+                        },
+                    ]}
+                >
+                    <Input
+                        value={name}
+                        onChange={updateName}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Musclegroup"
+                    name="musclegroup"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input a muscle group!',
+                        },
+                    ]}
+                >
+                    <Input
+                        value={musclegroup}
+                        onChange={updateMusclegroup}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Notes"
+                    name="notes"
+                >
+                    <Input
+                        value={notes}
+                        onChange={updateNotes}
+                    />
+                </Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
+}
+
+export default CreateUser;
