d2d00d77671f69f58028a165af968a8100190599
Ben Stones <benstones99@gmail.com>
2022-08-24 22:05:11 +0100

    cleanup imports

--- src/components/editWorkout.js
+++ src/components/editWorkout.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import DatePicker from 'react-datepicker'
 import { useParams } from "react-router-dom";
 
 const EditWorkout = (props) => {
