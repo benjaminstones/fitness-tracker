3f1c5090141dfc6ec48f9f411b7bb60d1d62792c
Ben Stones <benstones99@gmail.com>
2022-12-10 23:33:46 +0000

    wip - fixing exercises not being saved

--- backend/models/workout.model.js
+++ backend/models/workout.model.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const exerciseSchema = new Schema ({
+  name: { type: String, required: true },
+  sets: { type: String, required: true },
+  reps: { type: String, required: true },
+  weight: { type: String, required: true },
+})
+
 const workoutsSchema = new Schema({
   username: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: String, required: true },
-  exercises: { type: Array, required: true },
+  exercises: [exerciseSchema],
   date: { type: Date, required: true },
 }, {
   timestamps: true,
